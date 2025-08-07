9f01aed8af90afe87b60206019a9ab9a5fdfb610
HamzaBouzaidi <Hamzabzd91@gmail.com>
2025-08-07 22:37:02 +0300

    trying to make the project and about buttons link to their actual parts

---
+++ js/main.js
@@ -0,0 +1,16 @@
+console.log('hello world');
+function scrollToProjects(){
+
+    const target    = document.getElementById('projects')
+      if (target) {
+    target.scrollIntoView({ behavior: "smooth" });
+  }
+
+
+}
+function scrollToProjects1(){
+  const target1 = document.getElementById('about')
+  if (target){
+    target.scrollIntoView({ behavior: "smooth" });
+  }
+}
