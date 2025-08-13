37097cb511a66b19d7b5f65151b4fcbf276b95dd
HamzaBouzaidi <Hamzabzd91@gmail.com>
2025-08-13 12:45:55 +0300

    i have split the page and made it able to scroll
    have split the page to two fixing the problem of the heading being dragged down with scroll

--- js/main.js
+++ js/main.js
@@ -10,3 +10,23 @@ document.addEventListener('mousemove', (e) => {
     circle.style.left = cursorx - circleSize / 2 + 'px';
     circle.style.top = cursory - circleSize / 2 + 'px';
 });
+
+function scrollToProjects() {
+  const rightPart = document.querySelector('.Rightpart');
+  const target = rightPart.querySelector('#scrollIntoView'); // your project section inside Rightpart
+  if (target) {
+    rightPart.scrollTo({
+      top: target.offsetTop,
+      behavior: 'smooth'
+    });
+  }
+}
+
+function scrollToTopRightPart() {
+  const rightPart = document.querySelector('.Rightpart');
+  rightPart.scrollTo({ top: 0, behavior: 'smooth' });
+}
+const leftPart = document.querySelector('.Leftpart');
+const rightPart = document.querySelector('.Rightpart');
+
+
